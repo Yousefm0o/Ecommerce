d1f9568a552a65597eb15c4affe95db6775c4042
Yousefm0o <yousefmoo660@gmail.com>
2024-07-04 10:11:49 +0300

    Add docs folder for GitHub Pages

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-O5P36CVZ.mjs";import"./chunk-RXRGLRHO.mjs";import"./chunk-SQHCWZRT.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-ELFB25RB.mjs";import"./chunk-RXRGLRHO.mjs";import"./chunk-SQHCWZRT.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
